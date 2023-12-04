8f19fb7a2303585232b0b5c7bd9fff9c8e5f2d0d
1gnacio27 <ignacio.manzano03@gmail.com>
2023-12-04 20:41:30 +0100

    Database & Command Handler

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+const { Client, IntentsBitField, Partials, GatewayIntentBits } = require('discord.js');
+const WOK = require('wokcommands');
+const path = require('path');
+const config = require('./config.json');
+require('dotenv/config');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent,
   ],
+
+  partials: [Partials.Channel],
 });
 
-client.once('ready', () => {
+global.client = client;
+global.emote = config.emotes;
+global.color = config.colors;
+
+client.on('ready', () => {
+  new WOK({
+    client,
+    commandsDir: path.join(__dirname, 'commands'),
+    featuresDir: path.join(__dirname, 'features'),
+    events: {
+      dir: path.join(__dirname, 'events'),
+    },
+    mongoUri: process.env.MONGO_URI,
+    testServers: ['691808360277737502'],
+    botOwners: ['740004290063630357'],
+    cooldownConfig: {
+      errorMessage: `${emote.timer} Debes esperar todavía {TIME}`,
+      botOwnerBypass: true,
+      dbRequired: 30
+    },
+  })
   console.log(`El bot se ha iniciado como ${client.user.tag}`);
 });
 
